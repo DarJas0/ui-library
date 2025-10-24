a35d78e33ce5ca0c67ff9e47f47f5b8b2174ed87
dardan <dardan.jasari@dgtls.com>
2025-10-24 14:37:28 +0200

    tailwind

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import tailwind from "@tailwindcss/vite";
 
 const config: StorybookConfig = {
   "stories": [
@@ -14,6 +15,10 @@ const config: StorybookConfig = {
   "framework": {
     "name": "@storybook/react-vite",
     "options": {}
-  }
+  },
+  "viteFinal": async (config) => {
+    config.plugins = [...(config.plugins || []), tailwind()];
+    return config;
+  },
 };
 export default config;
